3ef0ae1d8f22eeb74b0c7b5905a5f15f67f19d8c
OdvutCoding <106329012+OdvutCoding@users.noreply.github.com>
2022-10-27 16:38:33 +0600

    Pushing changes on lab 2 task file

--- Mid Term/Lab Task 2/Lab2Task.php
+++ Mid Term/Lab Task 2/Lab2Task.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:135ae4f32f53ed76945623d1b47777266cd2396c8655325108cead86fef70b54
-size 6083
+oid sha256:396da10cb7c44d6fbe879b71e3d72a1dd9509c680b28ab511ad57ec7e4e6649a
+size 7060
